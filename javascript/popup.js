c59727d54c73f11571fdfb71a8c1714861322cf2
Edward Lando <edwardlando8@gmail.com>
2012-08-25 15:45:15 -0700

    folders

---
+++ javascript/popup.js
@@ -0,0 +1,11 @@
+function click(e) {
+  
+  window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
+});
